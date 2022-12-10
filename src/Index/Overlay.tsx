e93c4c931af9e8a349502432ca868a5492c66dd3
yinpo <347972265@qq.com>
2022-12-11 02:45:50 +0800

    fix(login): 修复页面顺序问题

--- src/Index/Overlay.tsx
+++ src/Index/Overlay.tsx
@@ -19,17 +19,17 @@ export default function (props: PropsType) {
     <OverlayContainer>
       <Overlay>
         <OverlayPanel id="panel-left">
-          <h2>welcome back!</h2>
-          <p>To keep connected with us please login with your personal info</p>
-          <Button id="sign-in" onClick={() => props.handleActive('')}>
-            sign in
+          <h2>hello friend!</h2>
+          <p>Enter your personal details and start journey with us</p>
+          <Button id="sign-up" onClick={() => props.handleActive('')}>
+            sign up
           </Button>
         </OverlayPanel>
         <OverlayPanel id="panel-right">
-          <h2>hello friend!</h2>
-          <p>Enter your personal details and start journey with us</p>
-          <Button id="sign-up" onClick={() => props.handleActive('active')}>
-            sign up
+          <h2>welcome back!</h2>
+          <p>To keep connected with us please login with your personal info</p>
+          <Button id="sign-in" onClick={() => props.handleActive('active')}>
+            sign in
           </Button>
         </OverlayPanel>
       </Overlay>
